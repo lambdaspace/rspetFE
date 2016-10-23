a8b7d8f9ae13445c2d832cddada1706b66dbf37f
Achilles Kaloeridis <achilleas90gr@gmail.com>
2016-10-23 20:52:19 +0300

    adds git ignore & list component

---
+++ front-end/src/components/HostsList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hosts">
+Super cool
+
+  </div>
+</template>
+
+<script>
+  export default {
+    created () {
+      this.getHosts()
+    },
+    data () {
+      return {
+        hosts: {}
+      }
+    },
+    methods: {
+      getHosts () {
+        this.$http.get('http://localhost:5000/rspet/api/v1.0/hosts').then((response) => {
+          this.hosts = response.body.hosts
+          console.log(this.hosts)
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
