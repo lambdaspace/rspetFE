a02047ebe171a70ab3480aad8ed84e0cc45cf239
Alexandros <aldor94@gmail.com>
2016-11-07 09:07:06 +0200

    Declare esversion for jslint and add semicolons

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,23 +1,25 @@
-import Vue from 'vue'
-import App from './App'
+// jshint esversion: 6
 
-import VueRouter from 'vue-router'
-import VueResource from 'vue-resource'
-import HostsList from 'components/HostsList.vue'
+import Vue from 'vue';
+import App from './App';
 
-Vue.use(VueResource)
-Vue.use(VueRouter)
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import HostsList from 'components/HostsList.vue';
+
+Vue.use(VueResource);
+Vue.use(VueRouter);
 
 const routes = [
   { path: '/', component: HostsList }
-]
+];
 
 const router = new VueRouter({
   routes // short for routes: routes
-})
+});
 
 new Vue({
   el: '#app',
   render: h => h(App),
   router
-})
+});
