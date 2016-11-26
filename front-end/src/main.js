e505192ae194b367a854f5f83c1cb08caab7c089
Achilles Kaloeridis <achilleas90gr@gmail.com>
2016-11-26 18:20:31 +0200

    config vuex

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -2,11 +2,13 @@
 
 import Vue from 'vue';
 import App from './App';
+import Vuex from 'vuex';
 
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
 
 Vue.use(VueResource);
+Vue.use(Vuex);
 // Vue.use(VueRouter);
 
 // const routes = [
