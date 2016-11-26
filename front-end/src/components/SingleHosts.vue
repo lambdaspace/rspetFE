7537148c0caadd26e0f049bbfb0b532d23e37c98
Achilles Kaloeridis <achilleas90gr@gmail.com>
2016-11-26 18:14:27 +0200

    Adds SingleHost component & adds vuex

---
+++ front-end/src/components/SingleHosts.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hosts">
+    <h2 class='text-center'> {{ host.hostname }} </h2>
+        <div class="">
+          IP: {{ host.ip }}
+        </div>
+        <div class="">
+          Port: {{ host.port }}
+        </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    created () {
+      this.getHosts()
+    },
+    data () {
+      return {
+        hosts: {}
+      }
+    },
+    methods: {
+      getHosts () {
+        this.$http.get('http://localhost:5000/rspet/api/v1.0/hosts')
+        .then((response) => {
+          this.hosts = response.body.hosts
+          console.log(this.hosts)
+        })
+        .catch((response) => {
+          console.log(response);
+        });
+      }
+    }
+  }
+</script>
+
+<style>
+  .hosts{
+    background-color: #a3a3a3;
+    height: 100vh;
+    width: 100%;
+    border-radius: 5px;
+    border: 2px solid #CCC;
+    padding-top: .5em;
+  }
+  .hosts ul {
+    list-style: none;
+  }
+</style>
